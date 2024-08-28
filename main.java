3462e719839fa20afc85ff7cd0e5b8c503aefe86
Rydia T. <71350188+RydiaT@users.noreply.github.com>
2024-08-28 13:29:17 -0500

    Added Main file

---
+++ main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+        print("Hello!");
+    }
+
+    public static void print(Object o) {
+        System.out.println(o);
+    }
+}
